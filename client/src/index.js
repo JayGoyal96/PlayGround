208299e8105fb12b713020ee8c703d60545993aa
JayGoyal96 <jaygoel921@gmail.com>
2022-01-19 00:37:08 +0530

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TerminalEditor from './Terminal';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TerminalEditor/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ server/server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const http = require("http");
+const utf8 = require("utf8");
+const app = express();
+const serverPort = 4000;
+const server = http.createServer(app);
+
+const Docker = require("dockerode");
+
+server.listen(serverPort);
+
+//socket.io instantiation
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+var dockerid = "";
+//Socket Connection
+
+io.on("connection", function (socket) {
+  const docker = new Docker();
+  console.log("New client connected");
+  socket.on("disconnect", function () {
+    console.log("Client disconnected");
+    docker.getContainer(dockerid).kill();
+  });
+  docker.createContainer(
+    {
+      Image: "ubuntu",
+      Tty: true,
+      OpenStdin: true,
+      StdinOnce: false,
+      AutoRemove: true,
+    },
+    function (err, container) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      dockerid = container.id;
+      container.attach(
+        {
+          stream: true,
+          stdin: true,
+          stdout: true,
+          stderr: true,
+        },
+        function (err, stream) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          socket.on("data", function (data) {
+            stream.write(data);
+          });
+          stream.on("data", function (data) {
+            socket.emit("data", utf8.decode(data.toString("binary")));
+          })
+          .on("close", function() {
+            container.kill();
+          });
+          container.start(function (err, data) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("Container started");
+          });
+        }
+      );
+    }
+  );
+});
